24bd4a3e8f0acae611f968371d6728db81044705
CVxTz <mansaryounessecp@gmail.com>
2024-04-10 01:42:57 +0200

    center

--- llm_voice/main.py
+++ llm_voice/main.py
@@ -22,20 +22,22 @@ class AudioData:
 async def index():
     audio_data = AudioData()
 
-    audio_recorder = AudioRecorder(on_audio_ready=audio_data.set_audio_byte64)
+    with ui.row().classes("w-full justify-center"):
+        audio_recorder = AudioRecorder(on_audio_ready=audio_data.set_audio_byte64)
 
-    ui.button(
-        "Play",
-        on_click=lambda: audio_recorder.play_recorded_audio()
-        if audio_data.audio_byte64
-        else ui.notify("No data to Play"),
-    )
-    ui.button(
-        "Download",
-        on_click=lambda: ui.download(audio_data.get_audio_bytes(), "audio.ogx")
-        if audio_data.audio_byte64
-        else ui.notify("No data to download"),
-    )
+    with ui.row().classes("w-full justify-center"):
+        ui.button(
+            "Play",
+            on_click=lambda: audio_recorder.play_recorded_audio()
+            if audio_data.audio_byte64
+            else ui.notify("No data to Play"),
+        )
+        ui.button(
+            "Download",
+            on_click=lambda: ui.download(audio_data.get_audio_bytes(), "audio.ogx")
+            if audio_data.audio_byte64
+            else ui.notify("No data to download"),
+        )
 
 
 ui.run()
